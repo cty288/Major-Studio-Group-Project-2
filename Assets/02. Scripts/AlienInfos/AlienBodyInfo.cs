086e568ac5fc06bf1b750c397061ea5ea8d4da1e
cty288 <tc3158@nyu.edu>
2022-11-11 02:46:50 -0500

    Update AlienBodyInfo.cs

--- Assets/02. Scripts/AlienInfos/AlienBodyInfo.cs
+++ Assets/02. Scripts/AlienInfos/AlienBodyInfo.cs
@@ -12,10 +12,14 @@ public enum AlienVoiceType {
 }
 
 public class AlienBodyInfo {
+    //https://stackoverflow.com/questions/35577011/custom-string-formatter-in-c-sharp
     public AlienBodyPartInfo HeadInfo;
     public AlienBodyPartInfo MainBodyInfo;
     public AlienBodyPartInfo LegInfo;
-    
+
+    private List<AlienBodyPartInfo> allBodyInfos = new List<AlienBodyPartInfo>();
+
+
     public AlienVoiceType VoiceType;
 
     public static bool instancesCreated = false;
@@ -36,6 +40,46 @@ public class AlienBodyInfo {
         mainBodyPartInstances = allBodyPartInstances.Where((info => info.BodyPartType == BodyPartType.Body)).ToList();
         legBodyPartInstances = allBodyPartInstances.Where((info => info.BodyPartType == BodyPartType.Legs)).ToList();
     }
+
+    public bool CheckContainTag<T>(out T tag) where T : class, IAlienTag {
+        tag = null;
+        bool hasTags = CheckContainTags(out List<T> tags);
+        if (hasTags) {
+            tag = tags[Random.Range(0, tags.Count)];
+        }
+        return hasTags;
+    }
+
+    public bool CheckContainTags<T>(out List<T> tags) where T : IAlienTag {
+        List<T> allTags = new List<T>();
+        foreach (var bodyInfo in allBodyInfos) {
+            foreach (IAlienTag tag in bodyInfo.Tags) {
+                if (tag is T t) {
+                    allTags.Add(t);
+                }
+            }
+        }
+        tags = allTags;
+        return allTags.Count > 0;
+    }
+
+    public float GetAverageFatness() {
+        float totalFatness = 0;
+        foreach (var bodyInfo in allBodyInfos) {
+            totalFatness += bodyInfo.HeightTag.Height;
+        }
+
+        return totalFatness / allBodyInfos.Count;
+    }
+
+    public float GetTotalHeight() {
+        float totalHeight = 0;
+        foreach (var bodyInfo in allBodyInfos) {
+            totalHeight += bodyInfo.HeightTag.Height;
+        }
+        return totalHeight;
+    }
+
     
     private AlienBodyInfo() {
 
@@ -45,6 +89,7 @@ public class AlienBodyInfo {
         MainBodyInfo = mainBodyPartInfo;
         LegInfo = legInfo;
         VoiceType = voiceType;
+        allBodyInfos = new List<AlienBodyPartInfo>() {headInfo, mainBodyPartInfo, legInfo};
     }
 
     public static AlienBodyInfo GetRandomAlienInfo() {
