78bd9f4a8ea23066c51e389dc7246a25bae0a6ed
cty288 <tc3158@nyu.edu>
2022-11-12 23:18:41 -0500

    Update VoiceTextSpliter.cs

--- Assets/02. Scripts/VoiceTextSpliter.cs
+++ Assets/02. Scripts/VoiceTextSpliter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public static class VoiceTextSpliter {
     private const int minSentenceLength = 36;
-    private static char[] splitBy = new[] { '.', '!', '?', ';', ':', '¡£','£¡', '£¿', '£»', '£º' };
+    private static char[] splitBy = new[] { '.', '!', '?', ';', ':'};
     public static List<string> Split(string text) {
         List<string> sentences = new List<string>();
         string[] splitText = text.Split(splitBy, StringSplitOptions.RemoveEmptyEntries);
