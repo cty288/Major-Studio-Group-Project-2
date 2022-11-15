8f961edd5518f1008d2e6f3b96a00a5cb30c076f
cty288 <tc3158@nyu.edu>
2022-11-15 15:37:23 -0500

    change

--- Assets/02. Scripts/BodyOutside/OutsideBodySpawner.cs
+++ Assets/02. Scripts/BodyOutside/OutsideBodySpawner.cs
@@ -5,11 +5,17 @@ using MikroFramework;
 using MikroFramework.Architecture;
 using MikroFramework.Event;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class OutsideBodySpawner : AbstractMikroController<MainGame> {
+public class OutsideBodySpawner : AbstractMikroController<MainGame>, ICanSendEvent {
     private AlienBody bodyViewController = null;
+    private BodyGenerationSystem bodyGenerationSystem;
+    [SerializeField] private Speaker speaker;
+
+    private bool speakEnd = false;
     private void Awake() {
-        this.GetSystem<BodyGenerationSystem>().CurrentOutsideBody.RegisterOnValueChaned(OnOutsideBodyChanged)
+        bodyGenerationSystem = this.GetSystem<BodyGenerationSystem>();
+        bodyGenerationSystem.CurrentOutsideBody.RegisterOnValueChaned(OnOutsideBodyChanged)
             .UnRegisterWhenGameObjectDestroyed(gameObject);
     }
 
@@ -17,6 +23,8 @@ public class OutsideBodySpawner : AbstractMikroController<MainGame> {
         if (body == null) {
             bodyViewController.Hide();
             this.Delay(0.5f, () => {
+                bodyViewController.onClickAlienBody -= OnOutsideBodyClicked;
+                
                 Destroy(bodyViewController.gameObject);
                 bodyViewController = null;
             });
@@ -25,6 +33,45 @@ public class OutsideBodySpawner : AbstractMikroController<MainGame> {
             bodyViewController = AlienBody.BuildShadowAlienBody(body, true).GetComponent<AlienBody>();
             bodyViewController.transform.position = transform.position;
             bodyViewController.Show();
+            bodyViewController.onClickAlienBody += OnOutsideBodyClicked;
         }
     }
+
+    private void OnOutsideBodyClicked() {
+        Debug.Log("Clicked");
+        if (bodyViewController.BodyInfo == bodyGenerationSystem.TodayAlien) {
+
+            LoadCanvas.Singleton.LoadUntil(() => {
+                speaker.Speak("Hahaha! I will kill you!", OnAlienSpeakEnd);
+            }, () => {
+                
+            }, () => false);
+        }
+        else {
+            LoadCanvas.Singleton.LoadUntil(() => {
+                speaker.Speak("Hey, I brought you some foods! Take care!", OnSpeakEnd);
+                this.GetSystem<PlayerResourceSystem>().AddFood(Random.Range(2, 4));
+            }, () => {
+
+            }, () => speakEnd);
+        }
+
+    }
+
+    private void OnAlienSpeakEnd() {
+        DieCanvas.Singleton.Show("You are killed by the creature!");
+    }
+
+    private void OnSpeakEnd() {
+        this.SendEvent<OnShowFood>();
+        this.Delay(4.5f, () => {
+            speakEnd = true;
+        });
+        
+    }
+}
+
+
+public struct OnShowFood {
+
 }
