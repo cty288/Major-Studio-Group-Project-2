b41ba1a2846b6dc35a247f4b2d5b7bba214fb728
SoulliesOfficial <77235731+SoulliesOfficial@users.noreply.github.com>
2022-11-15 01:21:37 -0500

    Revision Remainings

--- Assets/02. Scripts/Tests/TestAlien.cs
+++ Assets/02. Scripts/Tests/TestAlien.cs
@@ -5,21 +5,23 @@ using MikroFramework;
 using MikroFramework.Architecture;
 using UnityEngine;
 
-public class TestAlien : AbstractMikroController<MainGame> {
-    private void Start() {
-      //  Debug.Log(AlienDescriptionFactory.GetRadioDescription(BodyInfo.GetRandomBodyInfo(BodyPartDisplayType.Shadow, false), 1));
-        //Debug.Log(AlienDescriptionFactory.GetRadioDescription(BodyInfo.GetRandomBodyInfo( BodyPartDisplayType.Shadow, true), 0.5f));
+public class TestAlien : AbstractMikroController<MainGame>
+{
+    private void Start()
+    {
+        Debug.Log(AlienDescriptionFactory.GetRadioDescription(BodyInfo.GetRandomBodyInfo(BodyPartDisplayType.Shadow, false), 1));
+        Debug.Log(AlienDescriptionFactory.GetRadioDescription(BodyInfo.GetRandomBodyInfo(BodyPartDisplayType.Shadow, true), 0.5f));
 
-        
-        this.Delay(1f, () => {
-            /*
+
+        this.Delay(1f, () =>
+        {
             BodyInfo info1 = BodyInfo.GetRandomBodyInfo(BodyPartDisplayType.Shadow, false);
-            GameObject o1 = AlienBody.BuildShadowAlienBody(info1);
+            GameObject o1 = AlienBody.BuildShadowAlienBody(info1, false);
             o1.transform.position = new Vector3(0, 0, 0);
             Debug.Log(AlienDescriptionFactory.GetRadioDescription(info1, 0.3f));
 
             BodyInfo info2 = BodyInfo.GetBodyInfoOpposite(info1, 0.5f, 0.5f, true);
-            GameObject o2 = AlienBody.BuildShadowAlienBody(info2);
+            GameObject o2 = AlienBody.BuildShadowAlienBody(info2, false);
             o2.transform.position = new Vector3(10, 0, 0);
 
             BodyInfo news = BodyInfo.GetBodyInfoForDisplay(info1, BodyPartDisplayType.Newspaper);
@@ -28,7 +30,7 @@ public class TestAlien : AbstractMikroController<MainGame> {
 
             BodyInfo news2 = BodyInfo.GetBodyInfoForDisplay(info2, BodyPartDisplayType.Newspaper);
             GameObject o4 = AlienBody.BuildNewspaperAlienBody(news2, 1);
-            o4.transform.position = new Vector3(30, 0, 0);*/
+            o4.transform.position = new Vector3(30, 0, 0);
         });
     }
 }
