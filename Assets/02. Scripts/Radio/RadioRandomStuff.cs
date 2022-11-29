ee5b49d9a9393163e0aef2d4b99a1e24f774adbe
SoulliesOfficial <77235731+SoulliesOfficial@users.noreply.github.com>
2022-11-28 20:23:41 -0500

    Radio

--- Assets/02. Scripts/Radio/RadioRandomStuff.cs
+++ Assets/02. Scripts/Radio/RadioRandomStuff.cs
@@ -61,6 +61,11 @@ public  class RadioRandomStuff :MikroSingleton<RadioRandomStuff>, IController {
         RegisterRadioMessages(Message1);
         radioMessageCopies.AddRange(_radioMessages);
     }
+    
+    private string GetRandomString(params string[] stringArray)
+    {
+        return stringArray[Random.Range(0, stringArray.Length)];
+    }
 
     private RadioMessage Message1() {
         List<string> singers = new List<string>();
@@ -81,6 +86,44 @@ public  class RadioRandomStuff :MikroSingleton<RadioRandomStuff>, IController {
         return new RadioMessage(content, Random.Range(0.8f, 1.2f), Gender.MALE, Random.Range(0.2f, 0.8f), 1);
     }
     
+    private RadioMessage Message_MissingSomething()
+    {
+        string content = "Accroding to an informer, "
+                         + GetRandomString("their child is missing. ", "her wallet got robbed. ",
+                             "his pet dog is missing. ")
+                         + "We're not entirely sure yet that this was aliens did. But the possibility is extremely high.";
+        return new RadioMessage(content, Random.Range(0.8f, 1.2f), Gender.MALE, Random.Range(0.2f, 0.8f), 1);
+    }
+
+    private RadioMessage Message_WeatherReport()
+    {
+        string content = "Weather report of tomorrow. "
+                         + GetRandomString("In tomorrow morning ", "On tomorrow afternoon", "In tomorrow evening")
+                         + "The temperature will be "
+                         + GetRandomString("40", "41", "43", "44", "45", "47", "52", "57", "59", "62")
+                         + " Fahrenheit. Also, tomorrow day will be a "
+                         +GetRandomString("rainy", "windy", "cloudy", "sunny", "snowy")
+                         + " day.";
+        return new RadioMessage(content, Random.Range(0.8f, 1.2f), Gender.FEMALE, Random.Range(0.2f, 0.8f), 1);
+    }
+
+    private RadioMessage Message_Announcement_Of_President()
+    {
+        string content = "Our great President, Barack J Biden, announced that " +
+                         GetRandomString(
+                             "The whole country must raise the alert level.",
+                             "Troops are maintaining order in our vital cities.",
+                             "He fought an alien and won.",
+                             "He is forcing Congress to pass the bill. He claimed that we have run out of time to hesitate.");
+        return new RadioMessage(content, Random.Range(0.8f, 1.2f), Gender.MALE, Random.Range(0.2f, 0.8f), 1);
+    }
+
+    private RadioMessage Message_Alien()
+    {
+        string content = "This is Alien. For human. Surrender then live. Otherwise, die.";
+        return new RadioMessage(content, 0.5f, Gender.MALE, 1f, 1);
+    }
+
     private void RegisterRadioMessages(Func<RadioMessage> message) {
         _radioMessages.Add(message);
     }
